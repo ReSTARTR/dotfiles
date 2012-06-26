64b7e82934f1b15f4bc99bd0eeb1f67c100b712d
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2012-06-27 02:28:01 +0900

    install.sh : as for linux

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
+if [ ! -f ~/.vimrc ]; then
+    ln -s ~/dotfiles/.zshrc ~/.zshrc
+fi
+if [ ! -f ~/.screenrc ]; then
+    ln -s ~/dotfiles/.screenrc ~/.screenrc
+fi
+if [ ! -f ~/.gitconfig ]; then
+    ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+fi
+if [ ! -f ~/.tmux.conf ]; then
+    ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+fi
+if [ ! -f ~/.vimrc ]; then
+    ln -s ~/dotfiles/.vimrc ~/.vimrc
+fi
 
 if [ ! -d ~/.vim/bundle ]; then
     mkdir -p ~/.vim/bundle
 fi
 if [ ! -d ~/.vim/bundle/vundle ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-    vim -c BundleInstall
 fi
+vim -c BundleInstall
 
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -16,10 +31,4 @@ fi
 
 if [ ! -f ~/.zshrc ]; then
     ln -s ~/dotfiles/.zshrc ~/.zshrc
-fi
-if [ ! -f ~/.vimrc ]; then
-    ln -s ~/dotfiles/.zshrc ~/.zshrc
-fi
-if [ ! -f ~/.screenrc ]; then
-    ln -s ~/dotfiles/.screenrc ~/.screenrc
 fi
